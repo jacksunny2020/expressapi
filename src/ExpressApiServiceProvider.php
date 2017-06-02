90248ead9e7235c8a0a050fe9842e42d7cd37a45
jacksunny2020 <jacksunny2020@126.com>
2017-06-02 16:06:56 +0800

    Update ExpressApiServiceProvider.php

--- src/ExpressApiServiceProvider.php
+++ src/ExpressApiServiceProvider.php
@@ -35,18 +35,18 @@ class ExpressApiServiceProvider extends ServiceProvider {
     public function register() {
         include __DIR__ . '/routes.php';
         
-        //因为ApiController依赖于 SecurityServiceContract 和 OrderServiceContract 所以他们必须放在Controller之前初始化
-        $this->app->singleton('Jacksunny\ExpressApi\OrderServiceContract', function ($app) {
-            return new MockOrderService();
-        });
-        $this->app->singleton('Jacksunny\ExpressApi\SecurityServiceContract', function ($app) {
-            return new DefaultSecurityService();
-        });
-        $this->app->singleton('Jacksunny\ExpressApi\LogServiceContract', function ($app) {
-            return new MockLogService();
-        });
-        //因为ApiController依赖于 SecurityServiceContract 和 OrderServiceContract 所以必须放在他们之后初始化
-        $this->app->make('Jacksunny\ExpressApi\ApiController');
+//         //因为ApiController依赖于 SecurityServiceContract 和 OrderServiceContract 所以他们必须放在Controller之前初始化
+//         $this->app->singleton('Jacksunny\ExpressApi\OrderServiceContract', function ($app) {
+//             return new MockOrderService();
+//         });
+//         $this->app->singleton('Jacksunny\ExpressApi\SecurityServiceContract', function ($app) {
+//             return new DefaultSecurityService();
+//         });
+//         $this->app->singleton('Jacksunny\ExpressApi\LogServiceContract', function ($app) {
+//             return new MockLogService();
+//         });
+//         //因为ApiController依赖于 SecurityServiceContract 和 OrderServiceContract 所以必须放在他们之后初始化
+//         $this->app->make('Jacksunny\ExpressApi\ApiController');
         
     }
 
