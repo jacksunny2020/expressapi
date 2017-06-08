ec9ed0fbb80841945ae4e0bb2f8df86663ccea66
jacksunny2020 <jacksunny2020@126.com>
2017-06-08 15:37:20 +0800

    Create routes.php

--- src/routes.php
+++ src/routes.php
@@ -4,22 +4,22 @@
 //  'jacksunny\expressapi\ApiController@index');
 
 Route::get('api/status', 
-  'Jacksunny\Expressapi\ApiController@status');
+  'Jacksunny\ExpressApi\ApiController@status');
 
 //检查给定的签名是否正确
 ///api/check/sig?key=1&sig=11&data=hello
 Route::any('api/check/sig', 
-  'Jacksunny\Expressapi\ApiController@checkSig');
+  'Jacksunny\ExpressApi\ApiController@checkSig');
 
 //录入创建订单
 ///api/order/input?key=1&sig=11&order=hello
 Route::any('api/order/input', 
-  'Jacksunny\Expressapi\ApiController@inputOrder');
+  'Jacksunny\ExpressApi\ApiController@inputOrder');
 
 //查询指定条件的订单列表
 ///api/order/query?key=1&sig=11&query=hello
 Route::any('api/order/query', 
-  'Jacksunny\Expressapi\ApiController@queryOrders');
+  'Jacksunny\ExpressApi\ApiController@queryOrders');
 
 
 
